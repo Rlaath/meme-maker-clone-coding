364907d276f2c59ab66ed63d46b4b603270d07da
Sohee-laptop <sohee7170@naver.com>
2023-02-19 12:33:48 +0900

    line-width

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById('line-width')
 canvas.width=800;
 canvas.height=800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event){
@@ -11,7 +12,8 @@ function onMove(event){
     ctx.stroke();
     return;
   }
-
+  
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY)
 }
 function onPainting(){
@@ -21,7 +23,12 @@ function onPainting(){
 function canclePainting(){
   isPainting = false;
 }
+
+function lineChange(event){
+  ctx.lineWidth = event.target.value;
+}
 canvas.addEventListener('mousemove', onMove)
 canvas.addEventListener('mousedown', onPainting)
 canvas.addEventListener('mouseup', canclePainting)
 canvas.addEventListener('mouseleave',canclePainting)
+lineWidth.addEventListener('change', lineChange)
